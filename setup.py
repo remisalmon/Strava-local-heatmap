14677e1748298e424033cb260e9fd965bfc7e669
Marcel <marcel.haas@hhu.de>
2019-12-12 18:32:52 +0100

    ADD: setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Strava-local-heatmap',
+    version='',
+    packages=[''],
+    url='https://github.com/remisalmon/Strava-local-heatmap',
+    license='MIT',
+    author='remisalmon',
+    author_email='',
+    description='Python script to generate a high resolution heatmap from Strava GPX files'
+)
